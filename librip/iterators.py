8c5fe553922aecd2b7b8d950d399435a2f519d5f
lanavin <anastasia-p95@mail.ru>
2017-10-27 17:17:38 +0300

    выполнено задание №2

--- librip/iterators.py
+++ librip/iterators.py
@@ -1,17 +1,28 @@
 # Итератор для удаления дубликатов
-class Unique(object):
-    def __init__(self, items, **kwargs):
         # Нужно реализовать конструктор
         # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
         # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
         # Например: ignore_case = True, Aбв и АБВ разные строки
         #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
         # По-умолчанию ignore_case = False
-        pass
-
+class Unique(object):
+    def __init__(self, items, **kwargs):
+        self.ignore_case = kwargs.get('ignore_case', 'False')  # По-умолчанию ignore_case = False
+        if isinstance(items, list):
+            self.items = (x for x in items)
+        else:
+            self.items = items
+        self._s = set()
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
-
+        for a in self.items:
+            if self.ignore_case and isinstance(a, str):
+               if a.lower() not in map(lambda s: s.lower(), self._s):
+                  self._s.add(a)
+                  return a
+            elif a not in self._s:
+                self._s.add(a)
+                return a
+        else:
+            raise StopIteration
     def __iter__(self):
-        return self
+      return self
