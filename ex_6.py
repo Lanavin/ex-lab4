45aba7a5e5834f28c4a94f3d67b9370e65297ea3
Anastasia <lanavin>
2017-11-09 20:19:56 +0300

    all done

--- ex_6.py
+++ ex_6.py
@@ -6,7 +6,7 @@ from librip.decorators import print_result
 from librip.gens import field, gen_random
 from librip.iterators import Unique as unique
 
-path = None
+path = 'data_light_cp1251.json'
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
@@ -23,22 +23,23 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return list(sorted(unique(field(arg, 'job-name'), ignore_case=True)))
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda x: x.startswith('Программист'), arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda x: x + ' с опытом Python', arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    g = gen_random(100000, 200000, len(arg))
+    return list(map(lambda y: y[0] + y[1], zip(arg, list(map(lambda x: ', зарплата {} руб.'.format(x), g)))))
 
 
 with timer():
