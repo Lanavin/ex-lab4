5b6871d4eaf7c249041bd0f2cbe7c1d8da5b77e2
Anastasia <lanavin>
2017-11-09 18:38:33 +0300

    edited

--- ex_2.py
+++ ex_2.py
@@ -4,7 +4,7 @@ from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
-data3 = ['a', 'A', 'b', 'B']
+data3 = ['aAA', 'Aaa', 'bBB', 'Bbb']
 # Реализация задания 2
 print('list: ', ' '.join(map(str, Unique(data1))))
 print('random: ', ' '.join(map(str, Unique(data2))))
